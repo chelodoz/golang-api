e0726d14d508e17b66f84b61c369df802fd5d209
Marcelo Doz <marcelo_doz@outlook.com>
2022-05-29 23:02:46 -0300

    implement graceful shutdown

--- router/gorilla-mux.go
+++ router/gorilla-mux.go
@@ -1,8 +1,13 @@
 package router
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -31,6 +36,37 @@ func (*muxRouter) PATCH(uri string, f func(http.ResponseWriter, *http.Request)) 
 }
 
 func (*muxRouter) SERVE(port string) {
-	fmt.Printf("Mux HTTP Server running on port: %v", port)
-	http.ListenAndServe(port, muxDispatcher)
+
+	log := log.New(os.Stdout, "golang-api ", log.LstdFlags)
+	// create a new server
+	server := http.Server{
+		Addr:         port,              // configure the bind address
+		Handler:      muxDispatcher,     // set the default handler
+		ErrorLog:     log,               // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	}
+	go func() {
+		log.Printf("Mux HTTP Server running on port: %v", port)
+
+		err := http.ListenAndServe(port, muxDispatcher)
+		if err != nil {
+			log.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	signal.Notify(ch, syscall.SIGTERM)
+
+	// Block until a signal is received.
+	sig := <-ch
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	server.Shutdown(ctx)
 }
